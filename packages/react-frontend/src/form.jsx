4af77b3227334118340b177a8c2545fbf0ccc68d
Ava Romano <avaromano@Avas-MacBook-Pro-4.local>
2025-09-30 20:20:23 -0700

    nested functions

--- packages/react-frontend/src/form.jsx
+++ packages/react-frontend/src/form.jsx
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
 
-function Form(){
-    const [person, setPerson] = useState({ // ask him how this [person, setPerson] syntax works
-        name: "",
-        job: ""
-    });
+function Form(props){
+    // ask him how this [person, setPerson] syntax works
+    const [person, setPerson] = useState( {name: "", job: ""});
+
+    function handleChange(event){
+        const { name, value } = event.target;
+        if (name === "job")
+            setPerson({ name: person["name"], job: value });
+        else setPerson({ name: value, job: person["job"] });
+    }
+
+    function submitForm(){ // nested bc it uses person which is inside the Form function's scope
+        props.handleSubmit(person);
+        setPerson({ name: "", job: "" });
+    }
     
     return ( // question: how are you able to return an entire chunk of HTML
         <form>
@@ -24,15 +34,9 @@ function Form(){
                 value={person.job}
                 onChange={handleChange}
             />
+            <input type="button" value="Submit" onClick={submitForm} />
         </form>
     );
-}
-
-function handleChange(event){
-    const { name, value } = event.target;
-    if (name === "job")
-        setPerson({ name: person["name"], job: value });
-    else setPerson({ name: value, job: person["job"] });
 }
 
 export default Form;
--- packages/react-frontend/src/myapp.jsx
+++ packages/react-frontend/src/myapp.jsx
@@ -5,7 +5,18 @@ import Form from "./form";
 // define MyApp component as a function that returns what is supposed to be rendered on the screen
 // the <Table characterData=... /> line calls the Table component and gives it the character array
 function MyApp() {
-    const [characters, setCharacters] = useState([]);
+    const [characters, setCharacters] = useState([ ]);
+
+    function updateList(person){
+        setCharacters([...characters, person]); // what is this ellipsis doing
+    }
+
+    function removeOneCharacter(index){
+        const updated = characters.filter((character, i) => {
+            return i !== index;
+        });
+    setCharacters(updated); // set characters takes the new array you created by filtering as an argument, and will setCharacters (duh) 
+}
 
     return (
         <div className="container">
@@ -13,19 +24,12 @@ function MyApp() {
                 characterData={characters}
                 removeCharacter={removeOneCharacter}
             />
-            <Form />
+            <Form handleSubmit={updateList}/>
         </div>
     );
 }
 // the above HTML-looking syntax is allowed because we are writing in JSX. typical browsers don't accept it, so we
 // use Vite to 'transpile' it to standard JS/ECMAScript
-
-function removeOneCharacter(index){
-    const updated = characters.filter((character, i) => {
-        return i !== index;
-    });
-    setCharacters(updated); // set characters takes the new array you created by filtering as an argument, and will setCharacters (duh) 
-}
 
 // this makes the component available to be imported into other components or files
 export default MyApp;
